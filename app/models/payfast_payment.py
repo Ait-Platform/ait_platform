66e9c1302d3acce20451620d0c3379522aa5cd21
Sanjith Nanhoo <home@mathwithhands.com>
2025-11-16 09:11:37 +0200

    payfast

--- app/models/payfast_payment.py
+++ app/models/payfast_payment.py
@@ -2,10 +2,29 @@
 import os
 
 class Config:
-    PAYFAST_MODE         = os.environ.get("PAYFAST_MODE", "sandbox")
+    # --- PayFast base config ---------------------------------
+    # sandbox toggle → used to derive PAYFAST_MODE if not set
+    PAYFAST_SANDBOX = os.environ.get("PAYFAST_SANDBOX", "true").lower() == "true"
+
+    # If PAYFAST_MODE isn’t set in env, derive it from PAYFAST_SANDBOX
+    PAYFAST_MODE = os.environ.get(
+        "PAYFAST_MODE",
+        "sandbox" if PAYFAST_SANDBOX else "live",
+    )
+
     PAYFAST_MERCHANT_ID  = os.environ.get("PAYFAST_MERCHANT_ID", "")
     PAYFAST_MERCHANT_KEY = os.environ.get("PAYFAST_MERCHANT_KEY", "")
     PAYFAST_PASSPHRASE   = os.environ.get("PAYFAST_PASSPHRASE", "")
-    PAYFAST_RETURN_URL   = os.environ.get("PAYFAST_RETURN_URL", "")
-    PAYFAST_CANCEL_URL   = os.environ.get("PAYFAST_CANCEL_URL", "")
-    PAYFAST_NOTIFY_URL   = os.environ.get("PAYFAST_NOTIFY_URL", "")
+
+    PAYFAST_RETURN_URL = os.environ.get(
+        "PAYFAST_RETURN_URL",
+        "https://mathwithhands.com/payments/success",
+    )
+    PAYFAST_CANCEL_URL = os.environ.get(
+        "PAYFAST_CANCEL_URL",
+        "https://mathwithhands.com/payments/checkout/cancel",
+    )
+    PAYFAST_NOTIFY_URL = os.environ.get(
+        "PAYFAST_NOTIFY_URL",
+        "https://mathwithhands.com/payments/notify",
+    )

34b4497213246afeb0177eba12e191e5deaffe02
Sanjith Nanhoo <home@mathwithhands.com>
2026-07-28 21:11:06 +0200

    welcome page4

--- app/models/auth.py
+++ app/models/auth.py
@@ -125,6 +125,7 @@ class AuthSubject(db.Model):
     trial_expired_endpoint = db.Column(db.String(128), nullable=True)
     admin_start_endpoint = db.Column(db.String(128), nullable=True)
     show_on_welcome = db.Column(db.Boolean, nullable=False, default=False)
+
     
     def __repr__(self):
         return f"<AuthSubject slug={self.slug} name={self.name}>"

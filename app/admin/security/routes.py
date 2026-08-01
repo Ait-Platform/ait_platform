b68d3cd262712e577fde4df765d5b5fc603e841a
Sanjith Nanhoo <home@mathwithhands.com>
2026-08-01 15:31:01 +0200

    fix: import session in security routes

--- app/admin/security/routes.py
+++ app/admin/security/routes.py
@@ -1,4 +1,4 @@
-from flask import current_app, flash, redirect, render_template, request, url_for
+from flask import current_app, flash, redirect, render_template, request, url_for, session
 from flask_login import login_required
 from sqlalchemy import text
 

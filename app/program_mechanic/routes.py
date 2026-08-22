5bd14cb099045f1553992e1d50ac558237bc9ff6
Sanjith Nanhoo <home@mathwithhands.com>
2026-08-22 11:59:16 +0200

    Add Banking Details block to the internal Job Card dashboard view

--- app/program_mechanic/routes.py
+++ app/program_mechanic/routes.py
@@ -466,7 +466,11 @@ def job_card_detail(id):
 
     from app.models.mechanic import MechShop
     active_shop = MechShop.query.filter_by(user_id=current_user.id, onboarding_status='active').first()
-    return render_template("program_mechanic/job_card.html", job_card=job_card, today_date=today_date, communications=communications, client_debtor=client_debtor, shop=active_shop)
+    
+    from app.models.debtors import BusinessBankAccount
+    bank_account = BusinessBankAccount.query.filter_by(user_id=current_user.id).order_by(BusinessBankAccount.is_default.desc()).first()
+    
+    return render_template("program_mechanic/job_card.html", job_card=job_card, today_date=today_date, communications=communications, client_debtor=client_debtor, shop=active_shop, bank_account=bank_account)
 
 
 
